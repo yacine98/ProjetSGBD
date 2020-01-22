4cbb5a4bfb702150f3b988ceb00f7386adc0e53e
yacine98 <yacinediadia98@gmail.com>
2020-01-22 11:48:44 +0000

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import kyasql
 import os
+
 class ClientThread(threading.Thread):
     def __init__(self, ip, port, clientsocket):
         threading.Thread.__init__(self)
@@ -13,32 +14,19 @@ class ClientThread(threading.Thread):
     def run(self):
         base="."    
         print("Connexion de %s %s" % (self.ip, self.port, ))
-        r = self.clientsocket.recv(100000)
-        info_connect = r.decode()
-        info_connect = kyasql.space(info_connect)     
-        requete = kyasql.cut(info_connect,"#")   
-        if requete[0].upper() == "KYASQL":
-            if requete[1].upper() == "-U":
-                user = requete[2]
-                if requete[3].upper() == "-P": 
-                    password = requete[4]
-                    auth = kyasql.auth(user,password)
-                    self.clientsocket.send(auth.encode())
-        b=auth
-        if b == "1":
+        r1 = self.clientsocket.recv(100000)
+        r=r1.decode()
+        if r == "1":
             while True:
                 r = self.clientsocket.recv(100000)
                 chaine = r.decode()
                 chaine = kyasql.space(chaine)
-                #print(chaine)
+                
                 requete = kyasql.cut(chaine,"#")
-                #print(requete)
+                
                 if requete[0].upper() == "CREATE":
                     create=kyasql.create(requete[2],requete[1],base)
                     self.clientsocket.send(create.encode())    
-                    #print("ok")      
-                    #if requete[1].upper() == "Table" or requete[1] == "U":
-                    #   pass
                 if requete[0].upper() == "DROP":
                     drop = kyasql.drop(requete[1])
                     self.clientsocket.send(drop.encode())    
@@ -51,10 +39,10 @@ class ClientThread(threading.Thread):
                     else:
                         insert = "veuillez selectionner une base"
                         self.clientsocket.send(insert.encode())
-                if requete[0].upper() == "UPDATE":
-                    pass
-                if requete[0].upper() == "DELETE":
-                    pass
+                if requete[0].upper() == "ADDUSER":
+                    kyasql.adduser(requete[1],requete[2])
+                    resultat="Utilisateur créé"
+                    self.clientsocket.send(resultat.encode()) 
                 if requete[0].upper() == "SELECT":
                     if base != ".":
                         select = kyasql.select(requete[1],requete[3],base)
@@ -62,21 +50,13 @@ class ClientThread(threading.Thread):
                     else:
                         select = "veuiller selectionner une base"
                         self.clientsocket.send(select.encode()) 
-                if requete[0].upper() == "ADDUSER":
-                    pass
-                if requete[0].upper() == "DELUSER":
-                    pass
-                if requete[0].upper() == "QUIT":
-                    pass
-                if requete[0].upper() == "START":
-                    pass
-                if requete[0].upper() == "COMMIT":
-                    pass
-                if requete[0].upper() == "ROLLBACK":
-                    pass
+                
                 if requete[0].upper() == "USE":
                     base = requete[1]
-                    use = base+" now selected";
+                    if os.path.exists(base):
+                        use = base+" now selected";
+                    else:
+                        use = base+" don't exist";
                     self.clientsocket.send(use.encode()) 
                 if requete[0].upper() == "DESCRIBE":
                     if base != ".":
